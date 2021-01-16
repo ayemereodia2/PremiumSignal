7bf1df43bc199d64b30f16c3f5c3ad2d93bd76fb
Ayemere Odia <odiadavid2@gmail.com>
2021-01-16 19:15:15 +0100

    added

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.lightBlue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: LoginScreen(),
--- lib/presentation/screens/login_screen.dart
+++ lib/presentation/screens/login_screen.dart
@@ -9,47 +9,80 @@ import 'package:zenithbankkyc/presentation/utilities/toast_alert.dart';
 
 class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
-      child: new Scaffold(
-          resizeToAvoidBottomPadding: false,
-          body: BlocProvider(
-            create: (context) {
-              return LoginBloc();
-            },
-            child: BlocBuilder<LoginBloc, OnValidationSuccess>(
-              buildWhen: (previous, current) =>
-                  previous.username != current.username,
-              builder: (context, state) {
-                return Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    //   crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      //   SizedBox(height: 40.0),
-                      buildZenithLogo(),
-                      Container(
-                        padding:
-                            EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
-                        child: Column(
-                          children: <Widget>[
-                            userNameTextField(context, state),
-                            SizedBox(height: 20.0),
-                            passwordTextField(context, state),
-                            SizedBox(height: 50.0),
-                            _LoginButton(),
-                            SizedBox(height: 20.0),
-                          ],
-                        ),
-                      ),
-                      SizedBox(height: 15.0),
-                    ],
-                  ),
-                );
+    double c_width = MediaQuery.of(context).size.width*0.8;
+
+    return MaterialApp(
+
+      home: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(image: AssetImage("assets/signap.png"), fit: BoxFit.cover)
+        ),
+
+        child: new Scaffold(
+            resizeToAvoidBottomPadding: false,
+            backgroundColor: Colors.transparent,
+            body: BlocProvider(
+              create: (context) {
+                return LoginBloc();
               },
-            ),
-          )),
+              child: BlocBuilder<LoginBloc, OnValidationSuccess>(
+                buildWhen: (previous, current) =>
+                    previous.username != current.username,
+                builder: (context, state) {
+                  return Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      //   crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        //   SizedBox(height: 40.0),
+                        buildZenithLogo(c_width),
+                        Container(
+                          padding:
+                              EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+                          child: Column(
+                            children: <Widget>[
+                              userNameTextField(context, state),
+                              SizedBox(height: 20.0),
+                              passwordTextField(context, state),
+                              SizedBox(height: 50.0),
+                              RaisedGradientButton(
+                                  child: Text(
+                                    'LOGIN',
+                                    style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),
+                                  ),
+                                  gradient: LinearGradient(
+                                    colors: <Color>[Colors.lightBlueAccent, Colors.blue],
+                                  ),
+                                  onPressed: (){
+                                    print('button clicked');
+                                  }
+                              ),
+                              SizedBox(height: 10.0),
+                              _RegisterButton(),
+                              SizedBox(height: 20.0),
+                              RaisedButton(child: Text(
+                                'New user? Register',
+                                style: TextStyle(color: Colors.purpleAccent,fontWeight: FontWeight.bold),
+                              ),
+                                gradient: LinearGradient(
+                                  colors: <Color>[Colors.white, Colors.white],
+                                ),
+                                onPressed: (){
+                                  print('button clicked');
+                              }
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 15.0),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            )),
+      ),
     );
   }
 
@@ -60,11 +93,11 @@ class LoginScreen extends StatelessWidget {
         decoration: InputDecoration(
             errorText: state.password.invalid ? 'invalid password' : null,
             counterText: '',
-            labelText: 'Pin+OTP',
+            labelText: 'Password',
             labelStyle: TextStyle(
                 fontFamily: 'Montserrat',
                 fontWeight: FontWeight.bold,
-                color: Colors.grey),
+                color: Colors.lightBlueAccent),
             focusedBorder: UnderlineInputBorder(
                 borderSide: BorderSide(color: Colors.red))),
         obscureText: true,
@@ -80,22 +113,24 @@ class LoginScreen extends StatelessWidget {
           labelStyle: TextStyle(
               fontFamily: 'Montserrat',
               fontWeight: FontWeight.bold,
-              color: Colors.grey),
+              color: Colors.lightBlueAccent),
           focusedBorder:
               UnderlineInputBorder(borderSide: BorderSide(color: Colors.red))),
       onChanged: (username) =>
           context.bloc<LoginBloc>().add(UsernameChanged(username)),
     );
   }
+  Container buildZenithLogo(double width) {
 
-  Container buildZenithLogo() {
     return Container(
-      width: 100.00,
-      height: 100.00,
-      decoration: new BoxDecoration(
-          image: new DecorationImage(
-              image: ExactAssetImage('assets/zenithLogo.png'),
-              fit: BoxFit.fitHeight)),
+      padding: const EdgeInsets.fromLTRB(5,30,100,100),
+      width: width,
+      child: new Column (
+        children: <Widget>[
+          new Text ("PREMIUM", textAlign: TextAlign.left,style: TextStyle(color: Colors.lightBlueAccent,fontSize: 30.0, fontWeight: FontWeight.bold ),),
+          new Text ("SIGNALS..", textAlign: TextAlign.left,style: TextStyle(color: Colors.lightBlueAccent,fontSize: 30.0,fontWeight: FontWeight.bold)),
+        ],
+      ),
     );
   }
 
@@ -125,7 +160,7 @@ class _LoginButton extends StatelessWidget {
                 height: 45.0,
                 minWidth: 200.0,
                 child: FlatButton(
-                  color: Colors.red,
+                  color: Colors.lightBlueAccent,
                   disabledColor: Colors.grey,
                   key: const Key('loginForm_continue_raisedButton'),
                   child: Center(
@@ -146,6 +181,131 @@ class _LoginButton extends StatelessWidget {
                 ),
               );
       },
+    );
+  }
+}
+
+
+class _RegisterButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<LoginBloc, OnValidationSuccess>(
+      listener: (context, state) {
+        if (state is LogInSuccessState) {
+          Navigator.pushReplacementNamed(context, SHOW_LIST_PATH);
+        }
+        if (state is ErrorState) {
+          showErrorMessage(context, state.errorMessage);
+          //   Navigator.pushReplacementNamed(context, SHOW_LIST_PATH);
+        }
+      },
+      buildWhen: (previous, current) => previous.status != current.status,
+      builder: (context, state) {
+        return state.status.isSubmissionInProgress
+            ? CircularProgressIndicator()
+            : ButtonTheme(
+          height: 25.0,
+          minWidth: 200.0,
+          child: FlatButton(
+            color: Colors.white,
+            key: const Key('loginForm_continue_raisedButton'),
+            child: Center(
+              child: Text(
+                'Forgot Password?.',
+                style: TextStyle(
+                    color: Colors.black54,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Montserrat'),
+              ),
+            ),
+            onPressed: state.status.isValidated
+                ? () {}
+                : null,
+          ),
+        );
+      },
+    );
+  }
+}
+
+
+class RaisedGradientButton extends StatelessWidget {
+  final Widget child;
+  final Gradient gradient;
+  final double width;
+  final double height;
+  final Function onPressed;
+
+  const RaisedGradientButton({
+    Key key,
+    @required this.child,
+    this.gradient,
+    this.width = double.infinity,
+    this.height = 50.0,
+    this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: 50.0,
+      decoration: BoxDecoration(gradient: gradient, boxShadow: [
+        BoxShadow(
+          color: Colors.grey[500],
+          offset: Offset(0.0, 1.5),
+          blurRadius: 1.5,
+        ),
+      ]),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+            onTap: onPressed,
+            child: Center(
+              child: child,
+            )),
+      ),
+    );
+  }
+}
+
+
+class RaisedButton extends StatelessWidget {
+  final Widget child;
+  final Gradient gradient;
+  final double width;
+  final double height;
+  final Function onPressed;
+
+  const RaisedButton({
+    Key key,
+    @required this.child,
+    this.gradient,
+    this.width = double.infinity,
+    this.height = 20.0,
+    this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: 50.0,
+      decoration: BoxDecoration(gradient: gradient, boxShadow: [
+        BoxShadow(
+          color: Colors.grey[500],
+          offset: Offset(0.0, 1.5),
+          blurRadius: 1.5,
+        ),
+      ]),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+            onTap: onPressed,
+            child: Center(
+              child: child,
+            )),
+      ),
     );
   }
 }
